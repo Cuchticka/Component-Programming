c165f78b3db1b81ec0eae453a4599ad2eb3dd8ee
Monika Badovská <monika.badovska@student.tuke.sk>
2023-03-06 13:23:59 +0100

    Revert "Initial commit"
    This reverts commit a446f67fb0e4e06ee908c6c643919a863123627c

--- FieldState.java
+++
@@ -1,3 +0,0 @@
-public enum FieldState {
-    PLAYING, SOLVED
-}
--- Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        var field = new Field(3, 3);
-        var ui = new ConsoleUI(field);
-        ui.play();
-    }
-}
